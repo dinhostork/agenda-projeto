08084d77aa191e0e22f880221292dff3f78614ed
Dinho Stork <dinhostork@live.com>
2022-02-24 23:58:33 -0300

    Update api.js

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: "http://10.0.0.102:3333", //process.env.REACT_APP_API_URL,
+  baseURL: "http://localhost:3333", //process.env.REACT_APP_API_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
